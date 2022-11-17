d515d1750d8724b91512aaf534caa6d40a0458f5
Kripa Tailor <kripa@tailor.org>
2022-11-17 14:48:13 -0600

    Adding C101 and C102

--- C101 Factorial [Swift]/main.swift
+++ C101 Factorial [Swift]/main.swift
@@ -8,16 +8,16 @@ let n = 7
 
 
 // Add your code below:
-func factorial(n: Int)-> Int {   
-    print("Calculating factorial(\(n))...") 
+func factorial(n: Int)-> Int {   /// making a function called factorial  
+    print("Calculating factorial(\(n))...") /// printing the calculation factorai l
     if n <= 1{
-        print("Terminal case, returning 1.")
-        return 1             
+        print("Terminal case, returning 1.") /// returing terminal case 1
+        return 1             /// return 1
     }else{
-        let result = n * factorial(n:n - 1) 
-        print("Non-terminal case, returning \(result).")  
-        return result   
+        let result = n * factorial(n:n - 1)  /// n times the factorial 
+        print("Non-terminal case, returning \(result).")  /// returning the non-terminal result
+        return result   /// return result
     }
 
 }
-print("The final result is: \(factorial(n:n)).")  
+print("The final result is: \(factorial(n:n)).") //// printing the final result  
--- C102 Fibonacci [Swift]/main.swift
+++ C102 Fibonacci [Swift]/main.swift
@@ -9,17 +9,18 @@ let n = 7
 
 // Add your code below:
 
-func factorial(n: Int) -> Int {   
-    print("Calculating fibonacci(\(n))...")  
+func factorial(n: Int) -> Int {    //// making a function called factorial   
+    print("Calculating fibonacci(\(n))...")   ////// printing the calculating fibonacci     
     
-    if(n <= 2) {     
-        print("Terminal case, returning 1.")  
-        return 1 
+    if(n <= 2) {       //// if n is less that or equal to  
+        print("Terminal case, returning 1.")   //// returning terminal case 1   
+        return 1 /// returning 1   
     }
     else{
-        let result = factorial(n:n-1) + factorial(n:n - 2)  
-        print("Non-terminal case, returning \(result).")  
-        return result 
+        let result = factorial(n:n-1) + factorial(n:n - 2)  //// letting result equal factorial   
+        print("Non-terminal case, returning \(result).")   ///// returning the result for the non terminal case 
+        return result ///// return result      
     }
 }
-print("The final result is: \(factorial(n:n)).")     
+print("The final result is: \(factorial(n:n)).")    //// printing the final result
+
